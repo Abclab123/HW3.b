6969940da1f93c7d51f98295f1e65d7180210b86
Abclab123 <Abclab123@gmail.com>
2023-12-04 04:29:01 +0800

    R11625015 remove test file

--- test.py
+++
@@ -1,5 +0,0 @@
-import random 
-vector_dim = 2
-vector_count = 10
-example_data_generator = map(lambda i: (f'id-{i}', [random.random() for _ in range(vector_dim)]), range(vector_count))
-print(*list(example_data_generator), sep="\n")
