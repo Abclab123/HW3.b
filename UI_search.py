53da0b6ea9a9d50de8e44b64ba84d799f82c44b6
Alan-Kuan <kuanhaoan@gmail.com>
2023-12-04 00:29:59 +0800

    chore: add the template of UI_search

---
+++ UI_search.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+class UI_search:
+    def __init__(self):
+        self.result = []
+    def UI(self):
+        print('enter keyword:')
+        # search bookDB
+        # write your code here
+    def search(self, keyword: str) -> list:
+        pass
