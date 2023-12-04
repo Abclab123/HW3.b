047e445eb69877b2055d895d9da20288dcbf0b68
Alan-Kuan <kuanhaoan@gmail.com>
2023-12-04 12:21:21 +0800

    feat: add a text input and a button

--- UI_search.py
+++ UI_search.py
@@ -1,11 +1,27 @@
 import streamlit as st
 
+# the UI for searching the vector store
 class UI_search:
     def __init__(self):
         self.result = []
+
     def UI(self):
-        print('enter keyword:')
-        # search bookDB
-        # write your code here
+        prev_query = ''
+        cols = st.columns([4, 1])
+        query = cols[0].text_input(
+            '',
+            placeholder='',
+            label_visibility='collapsed',
+        )
+        cols[1].button(
+            ':mag:',
+            on_click=self.search(query),
+        )
+
     def search(self, keyword: str) -> list:
         pass
+
+# the followings won't be executed if this file is imported as a module
+if __name__ == '__main__':
+    ui = UI_search()
+    ui.UI()
