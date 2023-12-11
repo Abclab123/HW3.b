e5a01d237220aeb1257b482bb75638ce46a38baf
Rex-Shih <pipirex77@gmail.com>
2023-12-12 07:30:05 +0800

    good

---
+++ main.py
@@ -0,0 +1,14 @@
+from gtts import gTTS, lang
+
+
+class TextToSpeech:
+    def __init__(self, language: str):
+        self.language = language
+    def convert_to_audio(self, text, audio_path):
+        audio = gTTS(text=text, lang=self.language, slow=False)
+        audio.save(audio_path)
+        return audio
+def all_language():
+    dictionary = lang.tts_langs()
+    return dictionary
+    
